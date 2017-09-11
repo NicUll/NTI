30aea4d27bbfcdce932f7b630d531bad871e826a
NicUll <lordtardus@gmail.com>
2017-09-11 11:09:49 +0200

    Lektion Genomförd

--- Åk 3/V37/work/p5treeiterative_complete/sketch.js
+++ Åk 3/V37/work/p5treeiterative_complete/sketch.js
@@ -2,20 +2,22 @@ var angle = 0;
 var slider;
 
 function setup(){
-    createCanvas(400,400);
+    createCanvas(400,600);
     angle_slider = createSlider(0, PI/3, PI/8, 0.01);
-    len_slider = createSlider(0, 200, 100, 5);
+    len_slider = createSlider(0, 200, 100, 2);
     
 }
 
 function draw(){
     background(51);
+    
     angle = angle_slider.value();
     len = len_slider.value();
+    
     stroke(255);
     translate(200, height);
-    branch(len);
     
+    branch(len);   
 }
 
 function branch(len){
@@ -26,6 +28,7 @@ function branch(len){
         rotate(angle);
         branch(len*0.67);
         pop();
+        
         push();
         rotate(-angle);
         branch(len*0.67);
