15c41309229c4073977663f8513d641d6ee183d3
NicUll <lordtardus@gmail.com>
2017-10-20 12:22:20 +0200

    Revert "Revert "Merge branch 'master' into snake_comp""
    This reverts commit 206399f2024acd040f79149552f8aa03f7bfdc38.

--- Year3/V41/Snake/script/main.js
+++ Year3/V41/Snake/script/main.js
@@ -2,6 +2,7 @@
 var myGame;
 var directions = {[LEFT_ARROW]:[-1,0], [UP_ARROW]:[0,-1]};
 
+
 function centerCanvas() {
     var x = (windowWidth - width) / 2;
     var y = (windowHeight - height) / 2
@@ -15,17 +16,30 @@ function windowResized() {
 
 
 function setup() {
+    frameRate(20);
+    
     cnv = createCanvas(800, 600);
 
     background(0);
     centerCanvas();
+<<<<<<< HEAD
     /*
     myGame = new Game();
     
     myGame.start();
     */
+=======
+    
+    myGame = new Game(800, 600);
+    myGame.start();
+
+    
+>>>>>>> master
     fill(255);
     stroke(255);
+    
+    
+    
     
 
 }
