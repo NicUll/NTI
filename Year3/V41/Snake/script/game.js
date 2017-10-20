206399f2024acd040f79149552f8aa03f7bfdc38
NicUll <lordtardus@gmail.com>
2017-10-20 12:19:39 +0200

    Revert "Merge branch 'master' into snake_comp"
    This reverts commit 30bae6d27f96cdb2ff9e702156d790dc792fbbeb, reversing
    changes made to 5cabfca11bad4de34f747d99f9e19d7539753564.

--- Year3/V41/Snake/script/game.js
+++ Year3/V41/Snake/script/game.js
@@ -1,38 +1,14 @@
-var directions = {37:[-1,0], 38:[0,-1], 39:[1,0], 40:[0,1]};
-
-function Game(gWidth, gHeight) {
+function Game() {
     this.playing = false;
     this.lost = false;
-<<<<<<< HEAD
-=======
-    this.snake = new Snake(gWidth, gHeight);
-    
-    this.food = {x:null,y:null};
-    
-    this.genFood = function(){
-        this.food.x = Math.floor(Math.random()*(gWidth/SIZE));
-        this.food.y = Math.floor(Math.random()*(gHeight/SIZE));
-        
-    }
->>>>>>> master
 
     this.start = function() {
         this.playing = true;
-        this.genFood();
     }
 
     this.play = function(){
         if (this.playing) {
-            this.snake.show();
-            fill(0,255,0);
-            rect(this.food.x, this.food.y, SIZE, SIZE);
-            this.snake.move();
-            console.log(this.snake.head.x);
-            console.log(dist(this.snake.head.x, this.snake.head.y, this.food.x, this.food.y));
-            if(dist(this.snake.head.x, this.snake.head.y, this.food.x, this.food.y) < 3){
-                this.snake.eat();
-            }
-            
+            this.playField.update();
         }
     }
 
@@ -48,19 +24,9 @@ function Game(gWidth, gHeight) {
         this.lost = true;
     }
 
-<<<<<<< HEAD
     this.handleInput= function() {
     
-=======
-    this.handleInput = function() {
-        
-        //Sätter rätt riktning på snake-objektet
-        if(keyCode in directions){
-            this.snake.direction = directions[keyCode];
-        }
->>>>>>> master
 
     }
 
 }
-
--- Year3/V41/Snake/script/snake.js
+++ Year3/V41/Snake/script/snake.js
@@ -1,31 +1,15 @@
 const SIZE = 10;
-<<<<<<< HEAD
-=======
 
+const LEFT = 0;
+const UP = 1;
+const RIGHT = 2;
+const DOWN = 3;
 
->>>>>>> master
-
-
-
-<<<<<<< HEAD
 function Snake(x, y) {
     this.direction = RIGHT;
 
     this.x = x;
     this.y = y;
-=======
-/* Förklara på GitHub */
-//Rör den sig i x-led?
-
-
-function Snake(gWidth, gHeight) {
-    this.maxX = gWidth/SIZE;
-    this.maxY = gHeight/SIZE;
-    this.direction = [0,0];
-
-    //Börja med att skapa huvud
-    this.head = new Block(10,5);
->>>>>>> master
 
     this.body = [this.head]; //Lista med kropps-block
     
@@ -35,6 +19,17 @@ function Snake(gWidth, gHeight) {
         }
     }
     
+    this.changeDirection = function(){
+        if(keyCode == LEFT_ARROW){
+            this.direction = LEFT;
+        }else if(keyCode == RIGHT_ARROW){
+            this.direction = RIGHT;
+        }else if(keyCode == UP_ARROW){
+            this.direction = UP;
+        }else if(keyCode == DOWN_ARROW){
+            this.direction = DOWN;
+        }
+    }
     
     this.eat = function(){
         var lastBlock = this.body[this.body.length - 1]; //Sista blocket i listan, svansen
@@ -54,20 +49,15 @@ function Snake(gWidth, gHeight) {
         }
 
         //Vilken riktning har huvudet?
-        this.head.x += this.direction[0];
-        this.head.y += this.direction[1];
-        
-        //Kör runt kanten
-        if(this.head.x == this.maxX){
-            this.head.x = 0;
-        }else if(this.head.x == 0){
-            this.head.x = this.maxX;
-        }else if(this.head.y == this.maxY){
-            this.head.y = 0;
-        }else if(this.head.y == 0){
-            this.head.y = this.maxY;
+        if (this.direction == LEFT) {
+            this.head.x -= DIST;
+        } else if (this.direction == RIGHT) {
+            this.head.x += DIST;
+        } else if (this.direction == UP) {
+            this.head.y -= DIST;
+        } else {
+            this.head.y += DIST;
         }
-        
     }
 
 
@@ -81,8 +71,7 @@ function Block(x, y) {
     this.y = y;
 
     this.show = function () {
-        fill(255,255,255);
-        rect(this.x*SIZE, this.y*SIZE, SIZE, SIZE);
+        rect(this.x, this.y, SIZE, SIZE);
     }
 
 
