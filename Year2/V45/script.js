1c83e94bcb63b61f8935f32bc031e3629ef25565
NicUll <lordtardus@gmail.com>
2017-11-14 12:18:21 +0100

    Started week 45

---
+++ Year2/V45/script.js
@@ -0,0 +1,32 @@
+
+
+/* Syntax, referens
+https://betterexplained.com/articles/the-single-page-javascript-overview/
+
+Överlag:
+var variabelNamn = värde;
+var objektNamn = {nyckel:värde; nyckel2:värde; etc.};
+var listNamn = [värde1, värde2, etc]
+
+if(om det här är sant){
+    kör allt här inne;
+}
+
+while(medan detta är sant){
+    kör allt här inne;
+}
+
+function funktionsNamn(värde1_in, värde2_in, etc.){
+    kod som ska köras
+}
+
+köra funktion:
+funktionsNamn(värde1, värde2, etc.);
+
+console.log("saker att skriva"); skriver den texten
+console.log(variabelNamn); skriver ut värdet på variabeln
+console.log("massa text" + variabelnNamn); skriver ut texten följt av variabelvärdet
+*/
+
+
+//Din kod här under
