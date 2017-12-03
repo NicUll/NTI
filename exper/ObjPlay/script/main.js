69fb33e1b9cda4e1b25992ca33e8d63d008a98b4
Nic <lordtardus@gmail.com>
2017-12-03 13:23:44 +0100

    Added Graph

--- exper/ObjPlay/script/main.js
+++ exper/ObjPlay/script/main.js
@@ -6,3 +6,27 @@ button.onclick = function(){
     newDiv.innerText = "ost";
     display.appendChild(newDiv);
 }
+
+
+function Bar(id, label, value){
+    this.id = id;
+    this.label = label;
+    this.value = value;
+    /*
+    this.html = 
+    */
+}
+
+function Graph(htmlid, xlabel, ylabel){
+    this.id = htmlid;
+    this.xlabel = xlabel;
+    this.ylabel = ylabel;
+    this.bars = [];
+    
+    this.addBar = function(label, value){
+        var tempId = this.id + "-bar" + this.bars.length;
+        var tempBar = new Bar(tempId, label, value);
+        this.bars.push(tempBar);
+    }
+    
+}
