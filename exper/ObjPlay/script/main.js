f971c8243451e2f7c813139a46ee43da806ee47e
Nic <lordtardus@gmail.com>
2017-12-04 05:22:19 +0100

    Added weekly

--- exper/ObjPlay/script/main.js
+++ exper/ObjPlay/script/main.js
@@ -1,32 +1,57 @@
+
+
 var button = document.getElementById("add");
 var display = document.getElementById("display");
 
-button.onclick = function(){
-    var newDiv = document.createElement("div");
-    newDiv.innerText = "ost";
-    display.appendChild(newDiv);
+
+
+
+function createExtElement(type, id){
+    /*
+    var tempElement = '<' + type + ' id="' + id + '"></' + type + '>';*/
+    var tempElement = document.createElement(type);
+    tempElement.id = id;
+    return tempElement;
 }
 
 
-function Bar(id, label, value){
+function Bar(id, label, value, color){
     this.id = id;
     this.label = label;
     this.value = value;
-    /*
-    this.html = 
-    */
+    this.color = color;
+
+    this.htmlElement = createExtElement("div", this.id);
+    
 }
 
 function Graph(htmlid, xlabel, ylabel){
     this.id = htmlid;
     this.xlabel = xlabel;
     this.ylabel = ylabel;
-    this.bars = [];
+    this.bars = {income:[],expense:[]};
     
-    this.addBar = function(label, value){
-        var tempId = this.id + "-bar" + this.bars.length;
-        var tempBar = new Bar(tempId, label, value);
-        this.bars.push(tempBar);
+    this.addBar = function(type, label, value, color){
+        var tempId = this.id + "-bar" + this.bars[type].length;
+        var tempBar = new Bar(tempId, label, value, color);
+        console.log(typeof this.bars.type)
+        this.bars[type].push(tempBar);
+        return tempBar;
     }
     
+}
+
+
+
+var moneyGraph = new Graph("display", "Utgift", "Summa");
+
+button.onclick = function(){
+    var type = document.getElementById("type").value;
+    var category = document.getElementById("category").value;
+    var amount = document.getElementById("money").value;
+    var color = document.getElementById("color").value;
+
+    var newBar = moneyGraph.addBar(type, category, amount, color);
+    display.appendChild(newBar.htmlElement);
+    console.log(newBar);
 }
