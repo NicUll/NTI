9c46f95ad4f7171c311378fa14f4532315f79ea5
NicUll <lordtardus@gmail.com>
2017-12-04 16:55:27 +0100

    Fixed leading zero

--- Year2/V49/Textclock/Completed Clock/script.js
+++ Year2/V49/Textclock/Completed Clock/script.js
@@ -10,16 +10,22 @@ var secondElement = document.getElementById("seconds");
 
 
 
-
+function withLeadingZero(time){
+    var first = "";
+    if((time).toString().length < 2){
+        first = "0";
+    }
+    return first + time;
+}
 
 
 function updateClock(){
     
     var currentTime = new Date();
     
-    hourElement.textContent = currentTime.getHours();
-    minuteElement.textContent = currentTime.getMinutes();
-    secondElement.textContent = currentTime.getSeconds();
+    hourElement.textContent = withLeadingZero(currentTime.getHours());
+    minuteElement.textContent = withLeadingZero(currentTime.getMinutes());
+    secondElement.textContent = withLeadingZero(currentTime.getSeconds());
     
     
 }
