e8d02e4bd1bbc28cdf35c645036d04be64893324
NicUll <lordtardus@gmail.com>
2017-12-04 16:05:40 +0100

    Started week 49

---
+++ Year2/V49/script.js
@@ -0,0 +1,23 @@
+
+
+
+var hourElement = document.getElementById("hours");
+//Skapa minut och sekundelement här under
+
+
+var welcomeElement = document.getElementById("welcome-box");
+
+
+
+function updateClock(){
+    
+    /*Skriv kod som ska upprepas för att uppdatera klockan*/
+    
+}
+
+
+welcomeElement.innerText = "Da Klocka Is:";
+
+setInterval(updateClock, 3000);
+
+
